8954afca43a5b7684a6ca1ca03bc51ad944d6c3d
Ragu Raman Jayaraman <ragu@MacBook-Pro-2.local>
2020-05-31 10:55:00 -0400

    ragu asyncio test

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import os
 from os.path import join, dirname
 
 # Loading variables in .env folder
+print(__file__)
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
 
@@ -13,18 +14,18 @@ def main():
         host="10.244.2.52",
         user="root",
         passwd=os.environ.get("db_passwd"),
-        database="testdb" # remove this if running this py for the first time
+        # database="testdb_2" # remove this if running this py for the first time
     )
 
     mycursor = mydb.cursor()
-    #mycursor.execute("CREATE DATABASE testdb")
-    #list all databases
+    # mycursor.execute("CREATE DATABASE testdb_2")
+    # list all databases
     # mycursor.execute("SHOW DATABASES")
     # for db in mycursor:
     #     print(db)
 
     #Creating a table
-    #mycursor.execute("CREATE TABLE students (name VARCHAR(255), age INTEGER(3))")
+    # mycursor.execute("CREATE TABLE dogs (name VARCHAR(255), age INTEGER(2))")
     
     #Displaying tables
     # mycursor.execute("SHOW TABLES")
@@ -32,25 +33,25 @@ def main():
     #     print(tb)
 
     #inserting data into tables
-    # sql_ins = "INSERT INTO students (name, age) VALUES (%s, %s)"
-    # fav_student = [("ragu", 26),
-    #                 ("marshal", 67),
-    #                 ("dhivya", 27),
-    #                 ("deepu", 26)                
+    # sql_ins = "INSERT INTO dogs (name, age) VALUES (%s, %s)"
+    # fav_dog = [("caesar", 2),
+    #             ("rover", 7)
     # ]
 
-    # mycursor.executemany(sql_ins, fav_student)
-    mydb.commit()
+    # mycursor.executemany(sql_ins, fav_dog)
+    # mydb.commit()
 
     # Fetching data from database
-    mycursor.execute("SELECT * FROM students") # you can replace * with a column name
-    result = mycursor.fetchall()
-    for row in result:
-        print(row)
-    result = mycursor.fetchone()
-    print(result)
+    # mycursor.execute("SELECT * FROM dogs") # you can replace * with a column name
+    # result = mycursor.fetchall()
+    # for row in result:
+    #     print(row)
+    # mycursor.execute("SELECT * FROM dogs")
+    # result = mycursor.fetchone()
+    # print(result)
     
 
+    mycursor.execute("DROP DATABASE testdb_2") 
 
 if __name__ == "__main__":
     main()
